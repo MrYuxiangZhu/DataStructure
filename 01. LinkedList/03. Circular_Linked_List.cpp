944dce5016c7533ebcc8c27b839d145d7e6e33d4
YuxiangZHU <18842550358@163.com>
2021-03-20 23:00:19 +0800

    Update 03. Circular_Linked_List.cpp

--- 01. LinkedList/03. Circular_Linked_List.cpp
+++ 01. LinkedList/03. Circular_Linked_List.cpp
@@ -13,6 +13,23 @@ typedef struct List_Node
 	struct List_Node* next;	//指向下一个结点
 }List_Node, * LinkedList;
 
+//初始化结点，如同单链表的创建，需要先创建一个头结点并开辟内存空间，但与单链表不同的是
+//开辟内存空间成功后将头结点的next指向head自身
+LinkedList linked_list_init()
+{
+	List_Node* head = (List_Node*)malloc(sizeof(List_Node));
+	if (NULL == head)
+	{
+		cout << "创建失败！" << endl;
+		return NULL;
+	}
+	else
+	{
+		head->next = NULL;
+		return head;
+	}
+}
+
 int main()
 {
      
